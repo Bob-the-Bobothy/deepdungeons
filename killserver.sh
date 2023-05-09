725042c54abc44ab303f0d2985456f0455f1e503
Bob-the-Bobothy <bobothyinc@gmail.com>
2023-05-09 19:01:01 +0000

    Auto commit from server

--- killserver.sh
+++ killserver.sh
@@ -2,8 +2,8 @@
 
 rm -rf ~/deepdungeons/logs/killserver.log
 
-sudo kill $(sudo lsof -t -i:8000)
+sudo kill "$(sudo lsof -t -i:8000)"
 
 mkdir -p ~/deepdungeons/logs
 touch ~/deepdungeons/logs/killserver.log
-echo "Killed server at PID "$(sudo lsof -t -i:8000) >> ~/deepdungeons/logs/killserver.log
+echo "Killed server at PID ""$(sudo lsof -t -i:8000)" >> ~/deepdungeons/logs/killserver.log
